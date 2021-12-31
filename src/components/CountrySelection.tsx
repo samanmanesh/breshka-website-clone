b602ed2db0fdbb1f4d70cdc23f241889bd1eaa91
Saman Manesh <samanmanesh7@gmail.com>
2021-12-31 16:53:49 -0500

    finished country selection for now

--- src/components/CountrySelection.tsx
+++ src/components/CountrySelection.tsx
@@ -13,22 +13,6 @@ interface Props {
   setSelectedCountry;
 }
 
-// const countries = [
-//   "Canada",
-//   "USA",
-//   "Mexico",
-//   "United Kingdom",
-//   "France",
-//   "Germany",
-//   "Italy",
-//   "Spain",
-//   "Portugal",
-//   "Brazil",
-//   "Argentina",
-//   "Chile",
-//   "Colombia",
-//   "Ecuador",
-// ];
 interface ICountries {
   name: string;
   store: boolean;
@@ -92,7 +76,7 @@ export const CountrySelection = ({
         >
           {({ open }) => (
             <>
-              <Listbox.Button className=" rounded w-9/12 border border-black  h-10 relative flex items-center   ">
+              <Listbox.Button className=" rounded w-9/12 border border-black  h-10 relative flex items-center  ">
                 <label className=" font-light text-xs bg-white px-1 absolute bottom-8 left-4 ">
                   Market
                 </label>
@@ -103,7 +87,7 @@ export const CountrySelection = ({
                   ""
                 )}
 
-                <span className="w-full text-left ml-4 font-apercu-light font-medium text-sm">
+                <span className="w-full text-left ml-4 font-apercu-light font-medium text-sm ">
                   {selectedCountry}
                 </span>
                 {open ? (
@@ -113,7 +97,7 @@ export const CountrySelection = ({
                 )}
               </Listbox.Button>
 
-              <Listbox.Options className="bg-white drop-shadow-md absolute w-9/12 left-0 top-11 p-2 pt-11 rounded overflow-hidden">
+              <Listbox.Options className="bg-white drop-shadow-md absolute w-9/12 left-0 top-11  pt-11 rounded overflow-hidden">
                 <div className="absolute top-2 left-2 right-2">
                   <input
                     id="search-field"
@@ -147,16 +131,16 @@ export const CountrySelection = ({
                       <Listbox.Option
                         key={country.name}
                         value={country.name}
-                        className="flex items-center gap-2 p-2 w-full text-left font-apercu-light font-light text-sm "
+                        className="flex items-center gap-2 p-2 w-full text-left font-apercu-light font-light text-sm hover:bg-gray-50 cursor-pointer hover:font-bold"
                       >
-                        <span className="w-4 ">
+                        <span className="w-4 pl-2">
                           {country.store ? (
                             <ShoppingBagIcon className="w-3 h-3" />
                           ) : (
                             ""
                           )}
                         </span>
-                        <span className=" font-light text-xs  ">
+                        <span className="  text-xs  pl-3">
                           {country.name}
                         </span>
                       </Listbox.Option>
@@ -169,86 +153,5 @@ export const CountrySelection = ({
         </Listbox>
       </div>
     </div>
-    // <div>
-    //   <p className="text-md font-bold mb-6">
-    //     Select your location
-    //   </p>
-
-    //   <div className=" rounded w-9/12 border border-black p-1 relative ">
-    //     <span className=" font-light text-xs bg-white px-1 absolute bottom-7 left-4 ">
-    //       Market
-    //     </span>
-
-    //     <button
-    //       onClick={() =>
-    //         setShowSearchModule((prev) => !prev)
-    //       }
-    //       className="  flex p-1 w-full items-center  relative"
-    //     >
-    //       <span className=" flex items-center w-full content-between ">
-    //           <img
-    //             src="./clipboard.svg"
-    //             alt=""
-    //             aria-hidden
-    //             className="w-5 h-3 mx-2"
-    //           />
-
-    //         <span className="w-full  text-left  font-apercu-light font-semibold text-sm ">
-    //           {" "}
-    //           Canada
-    //         </span>
-    //       </span>
-
-    //       <img
-    //         src="./chevron-down.svg"
-    //         alt="chevron-down"
-    //         className="  w-4 h-4"
-    //       />
-    //     </button>
-
-    //     {showSearchModule && (
-    //       <div className=" bg-white drop-shadow-md absolute left-0 top-11 w-full h-80 p-2 rounded overflow-hidden">
-    //         <form
-    //           className=""
-    //           action="#"
-    //           method="GET"
-    //         >
-    //           {/* <label className="" htmlFor="search-field">
-    //                   Search
-    //                 </label> */}
-    //           <input
-    //             id="search-field"
-    //             className="block h-full w-full rounded bg-gray-100 border-transparent py-2 pl-4 pr-1 text-gray-900 placeholder-gray-300 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
-    //             placeholder="Search"
-    //             type="search"
-    //             name="search"
-    //             // onChange={}
-    //             // onKeyDown={}
-    //           />
-    //         </form>
-
-    //         <ul className="h-full w-full overflow-auto space-y-4 pt-4 pb-10">
-    //           {countries.map((country) => (
-    //             <li className="w-full">
-    //               <button className="flex items-center w-full ">
-    //                 <span className="w-full text-left font-apercu-light font-light text-sm flex gap-3  ">
-    //                   <span className="py-1">
-    //                     <img
-    //                       src="./clipboard.svg"
-    //                       alt="clipboard"
-    //                       title="clipboard"
-    //                       className="w-3 h-3 "
-    //                     />
-    //                   </span>
-    //                   <span>{country}</span>
-    //                 </span>
-    //               </button>
-    //             </li>
-    //           ))}
-    //         </ul>
-    //       </div>
-    //     )}
-    //   </div>
-    // </div>
   );
 };

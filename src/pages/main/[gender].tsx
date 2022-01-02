c2cf66df660986ab3cf50bfab7f56f5b5bb81e1b
Saman Manesh <samanmanesh7@gmail.com>
2022-01-02 15:38:57 -0500

    fixed routes

---
+++ src/pages/main/[gender].tsx
@@ -0,0 +1,16 @@
+import React, { ReactElement } from 'react'
+
+interface Props {
+  
+}
+
+export default function Gender({}: Props): ReactElement {
+  return (
+    <div>
+      Gender hi
+ catogry
+
+
+    </div>
+  )
+}

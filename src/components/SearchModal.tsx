680ecd57b177a34927c35095ee66aef4adeec865
Saman Manesh <samanmanesh7@gmail.com>
2022-01-02 17:39:28 -0500

    try to apply animation for search module

---
+++ src/components/SearchModal.tsx
@@ -0,0 +1,82 @@
+import { Fragment, useState } from "react";
+import {
+  Dialog,
+  Transition,
+} from "@headlessui/react";
+
+interface Props {}
+
+export default function SearchModal({
+  isOpen,
+  closeModal,
+  openModal,
+}) {
+  // let [isOpen, setIsOpen] = useState(true);
+
+  // function closeModal() {
+  //   setIsOpen(false);
+  // }
+
+  // function openModal() {
+  //   setIsOpen(true);
+  // }
+
+  return (
+    <>
+      {/* <div className="fixed inset-0 flex items-center justify-center">
+        <button
+          type="button"
+          onClick={openModal}
+          className="px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+        >
+          Open dialog
+        </button>
+      </div> */}
+
+      <Transition
+        appear
+        show={isOpen}
+        as={Fragment}
+      >
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-10 overflow-y-auto   "
+          onClose={closeModal}
+        >
+          <div className="min-h-screen  text-center ">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
+
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-100"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-100"
+            >
+              <div className=" w-full h-96  text-left   transform bg-white shadow-xl ">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
+                >
+                  Payment successful
+                </Dialog.Title>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
